c6e858e309667f23667880cdbe276767d57b74e5
Joel Pagliuca <174672965+jpag-canva@users.noreply.github.com>
2025-10-22 16:46:07 +1100

    draft solution

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -212,77 +212,15 @@ const _getQueryParam = (
   key?: string,
   multiple?: boolean
 ): string | undefined | Record<string, string> | string[] | Record<string, string[]> => {
-  let encoded
+  const urlObject = new URL(url)
+  const searchParams = new URLSearchParams(urlObject.search)
+  const getFn = multiple ? searchParams.getAll.bind(searchParams) : searchParams.get.bind(searchParams)
 
-  if (!multiple && key && !/[%+]/.test(key)) {
-    // optimized for unencoded key
-
-    let keyIndex = url.indexOf(`?${key}`, 8)
-    if (keyIndex === -1) {
-      keyIndex = url.indexOf(`&${key}`, 8)
-    }
-    while (keyIndex !== -1) {
-      const trailingKeyCode = url.charCodeAt(keyIndex + key.length + 1)
-      if (trailingKeyCode === 61) {
-        const valueIndex = keyIndex + key.length + 2
-        const endIndex = url.indexOf('&', valueIndex)
-        return _decodeURI(url.slice(valueIndex, endIndex === -1 ? undefined : endIndex))
-      } else if (trailingKeyCode == 38 || isNaN(trailingKeyCode)) {
-        return ''
-      }
-      keyIndex = url.indexOf(`&${key}`, keyIndex + 1)
-    }
-
-    encoded = /[%+]/.test(url)
-    if (!encoded) {
-      return undefined
-    }
-    // fallback to default routine
+  if (key !== undefined) {
+    return getFn(key) || undefined
   }
 
   const results: Record<string, string> | Record<string, string[]> = {}
-  encoded ??= /[%+]/.test(url)
-
-  let keyIndex = url.indexOf('?', 8)
-  while (keyIndex !== -1) {
-    const nextKeyIndex = url.indexOf('&', keyIndex + 1)
-    let valueIndex = url.indexOf('=', keyIndex)
-    if (valueIndex > nextKeyIndex && nextKeyIndex !== -1) {
-      valueIndex = -1
-    }
-    let name = url.slice(
-      keyIndex + 1,
-      valueIndex === -1 ? (nextKeyIndex === -1 ? undefined : nextKeyIndex) : valueIndex
-    )
-    if (encoded) {
-      name = _decodeURI(name)
-    }
-
-    keyIndex = nextKeyIndex
-
-    if (name === '') {
-      continue
-    }
-
-    let value
-    if (valueIndex === -1) {
-      value = ''
-    } else {
-      value = url.slice(valueIndex + 1, nextKeyIndex === -1 ? undefined : nextKeyIndex)
-      if (encoded) {
-        value = _decodeURI(value)
-      }
-    }
-
-    if (multiple) {
-      if (!(results[name] && Array.isArray(results[name]))) {
-        results[name] = []
-      }
-      ;(results[name] as string[]).push(value)
-    } else {
-      results[name] ??= value
-    }
-  }
 
   return key ? results[key] : results
 }
